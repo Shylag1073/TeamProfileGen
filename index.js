977a36577ce77bee283fc3b0ae6b1753b0c00d2f
shylag1073 <shylag1073@gmail.com>
2022-02-27 20:39:28 -0600

    adding to intern and enginer prompt

--- index.js
+++ index.js
@@ -24,43 +24,75 @@ inquirer.prompt([
         choices: ['add an enginer', 'add intern', ' none']
     }
 ]).then(teamData => {
-addingTeammates(teamData)
+    addingTeammates(teamData)
 })
 
 function addingTeammates(teamData) {
-
+    /// adding Enginer promtp////
     if (teamData.addTeam === 'add an enginer') {
-       
-            console.log("you're adding an engineer")
-        
 
-        inquirer.prompt({
-            type: 'input',
-            name: 'Engineer1',
-            message: "What is your engineer's name?"
-        }).then(engData => {
+        console.log("you're adding an engineer")
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'Engineer1',
+                message: "What is your engineer's name?"
+            },
+            {
+                type: 'input',
+                name: 'Eng1ID',
+                message: "What is your engineer's ID?"
+            },
+            {
+                type: 'input',
+                name: 'Eng1Email',
+                message: "What is your engineer's email?"
+            },
+            {
+                type: 'input',
+                name: 'Eng1Githib',
+                message: "What is your engineer's Github?"
+            }
+        ]).then(engData => {
             console.log(engData.Engineer1)
         })
 
     }
+    // Adding Intern propmt//////
+    else if (teamData.addTeam === 'add intern') {
 
-    else if (teamData.addTeam === 'add an enginer') {
-       
-        console.log("you're adding an engineer")
-    
-
-    inquirer.prompt({
-        type: 'input',
-        name: 'Engineer1',
-        message: "What is your engineer's name?"
-    }).then(engData => {
-        console.log(engData.Engineer1)
-    })
-
-}
+        console.log("you're adding an Intern")
 
 
- };
+        inquirer.prompt([
+            {
+                type: 'input',
+                name: 'Intern1',
+                message: "What is your Intern's name?"
+            },
+            {
+                type: 'input',
+                name: 'Int1ID',
+                message: "What is your Intern's ID?"
+            },
+            {
+                type: 'input',
+                name: 'Int1Email',
+                message: "What is your Intern's email?"
+            },
+            {
+                type: 'input',
+                name: 'Int1Githib',
+                message: "What is your Intern's Github?"
+            }
+        ]).then(IntData => {
+            console.log(IntData.Intern1)
+        })
+
+    }
+
+
+};
 
 
 
